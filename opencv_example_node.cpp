852b9485c7f42383f8c7aad74f11520c72d8b8c7
Jakub Muszyński <39170322+jakubmuszynski@users.noreply.github.com>
2018-05-11 23:09:44 +0200

    Update opencv_example_node.cpp

--- opencv_example_node.cpp
+++ opencv_example_node.cpp
@@ -21,76 +21,72 @@ bool first=1;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-  cv::Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
   int lowThreshold=10;
   int ratio = 3;
   int kernel_size = 3;
+  cv::Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
   cv::Mat hsv_image;
   cv::cvtColor(image, hsv_image, cv::COLOR_BGR2HSV);
   cv::Mat result;
   cv::inRange(hsv_image, cv::Scalar(90, 70, 70), cv::Scalar(130, 255, 255), result); // Finding blue color on image
   GaussianBlur( result, result, Size(9, 9), 2, 2 );  // Reduce the noise so we avoid false circle detection
   vector<Vec3f> circles;
-//  cv::Mat src_gray;
-//  cvtColor( image, src_gray, CV_BGR2GRAY );
-
+  //cv::Mat src_gray;
+  //cvtColor(image, src_gray, CV_BGR2GRAY);
   marker_size=0;
   HoughCircles( result, circles, CV_HOUGH_GRADIENT, 1, 1, 200, 50, 0, 0 );  // Apply the Hough Transform to find the circles
-  for( size_t i = 0; i < circles.size(); i++ )
+
+  for(size_t i = 0; i < circles.size(); i++)
   {
-      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-      int radius = cvRound(circles[i][2]);
-  	  marker_size = radius;
-//      circle( image, center, radius, Scalar(0,0,255), 3, 8, 0 );  // circle outline
-   }
+    Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+    int radius = cvRound(circles[i][2]);
+    marker_size = radius;
+    //circle( image, center, radius, Scalar(0,0,255), 3, 8, 0 );  // circle outline
+  }
 
-   // Prawdopodobienstwo wykrycia kola
-
-     if(circles.size()>0 && mana != MAX_MANA)
-     {
-       mana++;
-       prev_marker_size=marker_size;
-       prev_circles=circles;
-     }
-     else if(circles.size()>0)
-     {
-       prev_marker_size=marker_size;
-       prev_circles=circles;
-     }
-     else if(circles.size()==0 && mana>0)
-     {
-       marker_size=prev_marker_size;
-       circles=prev_circles;
-       mana--;
-     }
-
+  // Prawdopodobienstwo wykrycia kola
+  if(circles.size()>0 && mana != MAX_MANA)
+  {
+    mana++;
+    prev_marker_size=marker_size;
+    prev_circles=circles;
+  }
+  else if(circles.size()>0)
+  {
+    prev_marker_size=marker_size;
+    prev_circles=circles;
+  }
+  else if(circles.size()==0 && mana>0)
+  {
+    marker_size=prev_marker_size;
+    circles=prev_circles;
+    mana--;
+  }
 
   cv::Mat fin;
   fin= cv::Mat::zeros(result.size(), result.type());
   for( size_t i = 0; i < circles.size(); i++)
   {
-
-      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-      int radius = cvRound(circles[i][2]);
-      circle( fin, center, radius, 255, -1, 8, 0 );
-
+    Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+    int radius = cvRound(circles[i][2]);
+    circle( fin, center, radius, 255, -1, 8, 0 );
   }
 
   if(marker_size < 35 && marker_size>0)
   {
-	   GO = true;
-   }
-   else
-   {
-	    GO = false;
+	  GO = true;
+  }
+  else
+  {
+	  GO = false;
   }
 
-if(GO)
-{
-	  geometry_msgs::TwistPtr vel(new geometry_msgs::Twist());
-  	vel->linear.x=-0.5;
-  	vel->angular.z=0.0;
-//  	vel_pub.publish(vel);                                                        //uncomment for ride
+  if(GO)
+  {
+    geometry_msgs::TwistPtr vel(new geometry_msgs::Twist());
+    vel->linear.x=-0.5;
+    vel->angular.z=0.0;
+    //vel_pub.publish(vel);                      //uncomment for ride
     if(first==1)
     {
       cout << " Bzi";
@@ -100,25 +96,24 @@ if(GO)
     {
       cout<<"uu";
     }
-}
-else
-{
-  	geometry_msgs::TwistPtr vel(new geometry_msgs::Twist());
-  	vel->linear.x=-0.0;
-  	vel->angular.z=0.0;
-//  	vel_pub.publish(vel);                                                        //uncomment for ride
+  }
+  else
+  {
+    geometry_msgs::TwistPtr vel(new geometry_msgs::Twist());
+    vel->linear.x=-0.0;
+    vel->angular.z=0.0;
+    //vel_pub.publish(vel);                       //uncomment for ride
     if(first==0)
     {
       first=1;
       cout<<endl;
     }
-}
+  }
 
-//  imshow("Image",image);
+  //imshow("Image",image);
   imshow("Image edited", result);
   imshow("Image circle found",fin);
   waitKey(1);
-
 }
 
 int main(int argc, char **argv)
@@ -128,12 +123,12 @@ int main(int argc, char **argv)
   //initialize node
   ros::init(argc, argv, "cv_example");
 
-  // node handler
+  //node handler
   ros::NodeHandle n;
 
-  // subsribe topic
+  //subsribe topic
   ros::Subscriber sub = n.subscribe("/cv_camera/image_raw", 1, imageCallback);
-//  vel_pub = n.advertise<geometry_msgs::Twist>("cmd_joy", 1);                      //uncomment for ride
+  //vel_pub = n.advertise<geometry_msgs::Twist>("cmd_joy", 1);                      //uncomment for ride
 
   // publish
   image_transport::ImageTransport it(n);
